0d6344a504cec920111da1260aa830bf9d923397
Ar Ridho Gusti <ridhogusti0@gmail.com>
2020-09-13 19:10:31 +0700

    Updates

---
+++ precache-manifest.7573bbb5aa2b35ac2c6bd95aa41c6c2f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e6e8d0bec47013f2ae1e50a21f48de",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "dacfe2b01f9615d632d7",
+    "url": "/myportfolio/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "0d11c9a9ae1774b901ee",
+    "url": "/myportfolio/static/css/main.e8fc75ec.chunk.css"
+  },
+  {
+    "revision": "dacfe2b01f9615d632d7",
+    "url": "/myportfolio/static/js/2.3179ef46.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myportfolio/static/js/2.3179ef46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d11c9a9ae1774b901ee",
+    "url": "/myportfolio/static/js/main.8571895e.chunk.js"
+  },
+  {
+    "revision": "68061d5e6213492218ed",
+    "url": "/myportfolio/static/js/runtime-main.0d50133a.js"
+  },
+  {
+    "revision": "770fed7eafa5991c31f2ee3df728da99",
+    "url": "/myportfolio/static/media/dashboard_fastrata.770fed7e.png"
+  },
+  {
+    "revision": "d3c283662cbe997db7ecad91e213c84d",
+    "url": "/myportfolio/static/media/dashboard_kotakmakan.d3c28366.png"
+  },
+  {
+    "revision": "1ec5eca4e93b0348a388abde939067f0",
+    "url": "/myportfolio/static/media/landing_page.1ec5eca4.png"
+  },
+  {
+    "revision": "693886956342df1a2c84c19e3b2c4613",
+    "url": "/myportfolio/static/media/my_avatar.69388695.jpg"
+  },
+  {
+    "revision": "b80c6fc3482dd14dd24815dc675af9e2",
+    "url": "/myportfolio/static/media/ssflutix.b80c6fc3.jpg"
+  }
+]);
